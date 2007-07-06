c7125604b0b5a8212b6b132f6b869e443afebe69
jnjitkoff <jnjitkoff@0dd1add8-2234-0410-ac04-417ef3e27949>
2007-07-06 01:08:57 +0000

    added php opentag fix

--- trunk/Mac/Resources/www/common/dirlist1.php
+++ trunk/Mac/Resources/www/common/dirlist1.php
@@ -6,7 +6,7 @@ $dir = realpath($dir)
 
 <html>
 <head>
-	<title>Telekinesis - <?=$dir?></title>
+	<title><?=$_ENV["COMPUTER_NAME"]?> - <?=$dir?></title>
     <meta name="viewport" id="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 	<link rel="stylesheet" href="/css/style.css" type="text/css" media="screen" charset="utf-8" />
 	<script src="/js/remote.js" type="text/javascript" charset="utf-8" />
--- trunk/Mac/Resources/www/ipps/Camera/index.php
+++ trunk/Mac/Resources/www/ipps/Camera/index.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
 	<script>
 	function $(o) {return document.getElementById(o);}
 	</script>
