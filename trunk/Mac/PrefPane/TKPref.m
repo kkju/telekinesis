39822564939213160c69ae58effb1f794627c139
jnjitkoff <jnjitkoff@0dd1add8-2234-0410-ac04-417ef3e27949>
2007-07-07 04:16:02 +0000

    0.9.9

---
+++ trunk/Mac/PrefPane/TKPref.m
@@ -0,0 +1,18 @@
+//
+//  TKPref.m
+//  TK
+//
+//  Created by alcor on 7/6/07.
+//  Copyright (c) 2007 Google Inc. All rights reserved.
+//
+
+#import "TKPref.h"
+
+
+@implementation TKPref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
