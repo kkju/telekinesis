4ba97fa34bbf0c5bdb962c15d2238914f15f7f1d
jnjitkoff <jnjitkoff@0dd1add8-2234-0410-ac04-417ef3e27949>
2007-07-16 03:34:42 +0000

    0.9.21

--- trunk/Common/www/tapps/Remote.tapp/index.php
+++ trunk/Common/www/tapps/Remote.tapp/index.php
@@ -15,12 +15,12 @@
 <center><img src="buttons.png" width="192" height="299" border="0" alt="" usemap="#remote_Map">
 </center>
 	<map name="remote_Map">
-<area shape="circle" alt="" coords="96,261,36" onClick="loadURL('/t/runscript?path=scripts/Menu.scpt')" href="#">
-		<area shape="circle" alt="" coords="97,96,54" onClick="loadURL('/t/runscript?path=scripts/Play-Pause.scpt')" href="#">
-		<area shape="poly" alt="" coords="191,1,96,95,190,189" onClick="loadURL('/t/runscript?path=scripts/Next.scpt')" href="#">
-		<area shape="poly" alt="" coords="189,188,98,97,2,188" onClick="loadURL('/t/runscript?path=scripts/Minus.scpt')" href="#">
-		<area shape="poly" alt="" coords="1,189,97,98,0,0" onClick="loadURL('/t/runscript?path=scripts/Prev.scpt')" href="#">
-		<area shape="poly" alt="" coords="0,0,192,0,95,97" onClick="loadURL('/t/runscript?path=scripts/Plus.scpt')" href="#">
+<area shape="circle" alt="" coords="96,261,36" onClick="loadURL('/t/runscript?path=scripts/Menu.scpt&rand=' +  Math.random())" href="#">
+		<area shape="circle" alt="" coords="97,96,54" onClick="loadURL('/t/runscript?path=scripts/Play-Pause.scpt&rand=' +  Math.random())" href="#">
+		<area shape="poly" alt="" coords="191,1,96,95,190,189" onClick="loadURL('/t/runscript?path=scripts/Next.scpt&rand=' +  Math.random())" href="#">
+		<area shape="poly" alt="" coords="189,188,98,97,2,188" onClick="loadURL('/t/runscript?path=scripts/Minus.scpt&rand=' +  Math.random())" href="#">
+		<area shape="poly" alt="" coords="1,189,97,98,0,0" onClick="loadURL('/t/runscript?path=scripts/Prev.scpt&rand=' +  Math.random())" href="#">
+		<area shape="poly" alt="" coords="0,0,192,0,95,97" onClick="loadURL('/t/runscript?path=scripts/Plus.scpt&rand=' +  Math.random())" href="#">
 	</map>
 	</div>
 </body>
