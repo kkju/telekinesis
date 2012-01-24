94573a76fd835fcddb4018aba0ee03d6126373d3
paulpavlish <paulpavlish@0dd1add8-2234-0410-ac04-417ef3e27949>
2012-01-24 06:59:51 +0000

    Fixing issue 152, issue 153 getting the index and dirlist to work again

--- Common/www/index.php
+++ Common/www/index.php
@@ -1,12 +1,8 @@
-
-
-
-
 <html>
 <head>
-  <title><?=$_ENV["COMPUTER_NAME"]?></title>
-  <meta name="viewport" content="width=320, height=418" />
-  <link rel="stylesheet" href="css/menu.css" type="text/css" media="screen" charset="utf-8">
+<title><?=$_ENV["COMPUTER_NAME"]?></title>
+<meta name="viewport" content="width=320, height=418" />
+<link rel="stylesheet" href="css/menu.css" type="text/css" media="screen" charset="utf-8"/>
 <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-precomposed.png"/> 
 
 <meta name="application-name" content="Telekinesis"/>
@@ -15,23 +11,23 @@
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
-<link rel="icon" href="images/telekinesis_16.png" sizes="16x16">
-<link rel="icon" href="images/telekinesis_32.png" sizes="32x32">
-<link rel="icon" href="images/telekinesis_128.png" sizes="128x128"> 
-<link rel="icon" href="images/telekinesis_512.png" sizes="512x512"> 
+<link rel="icon" href="images/telekinesis_16.png" sizes="16x16"/>
+<link rel="icon" href="images/telekinesis_32.png" sizes="32x32"/>
+<link rel="icon" href="images/telekinesis_128.png" sizes="128x128"/> 
+<link rel="icon" href="images/telekinesis_512.png" sizes="512x512"/> 
 
-  <script src="js/menu.js" type="text/javascript" charset="utf-8"></script>
-<script src="/js/hidelocation.js" type="text/javascript" charset="utf-8" />
+<script src="js/menu.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/hidelocation.js" type="text/javascript" charset="utf-8"></script>
 </head>
 
-<?
+<?php
 
 $img_path = $_ENV["HOME"]."/Library/Application Support/iPhone Remote/Background.jpg";
 
 if (!file_exists($img_path))
 	$img_path = $_ENV["HOME"]."/Library/Application Support/iPhone Remote/Background.default.jpg";
 
-//echo $img_path;
+//echo $img_path
 if (!file_exists($img_path)) {
   echo '<body>';
 } else {
